f77a6de5b27dabf2f2887c68647ab8bca0439685
vilozio <korotkovkm@gmail.com>
2017-05-23 06:56:39 +0300

    Initial commit

---
+++ photo-service/src/main/java/ru/sbt/ds/PhotoController.java
@@ -0,0 +1,18 @@
+package ru.sbt.ds;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+
+import static java.nio.file.Files.readAllBytes;
+
+@RestController
+public class PhotoController {
+    @RequestMapping(value = "/img", produces = "image/jpeg")
+    public byte[] getById(long id) throws IOException {
+        return readAllBytes(new File("D:/Koala.jpg").toPath());
+    }
+}
